4aaf877dd7ce622bec6458123e06e646bd9edf8c
Carini108 <126793650+Carini108@users.noreply.github.com>
2024-05-08 02:02:00 +0200

    myscript.py

--- tutorials/myscript.py
+++ tutorials/myscript.py
@@ -29,7 +29,6 @@ for iev, event in enumerate(F):
         # antitop
         if part.pid == -6:
             antitop_momenta.append(lhe_parser.FourMomentum(part))
-
         ################    W    ################
         # W+
         if part.pid == 24:
@@ -37,7 +36,6 @@ for iev, event in enumerate(F):
         # W-
         if part.pid == -24:
             wminus_momenta.append(lhe_parser.FourMomentum(part))
-
         ################    b    ################
         # b
         if part.pid == 5:
@@ -48,17 +46,21 @@ for iev, event in enumerate(F):
     
 for i in range(len(top_momenta)):
     # find the top and antitop momenta
+    print('\nThe momenta of the byproducts should add up to the mother particle momentum:')
     print(top_momenta[i].pz-wplus_momenta[i].pz-b_momenta[i].pz)
     print(antitop_momenta[i].pz-wminus_momenta[i].pz-antib_momenta[i].pz)
     # Lorentz boosts
     # p.boost_to_restframe(q) boosts p to the system where q is zero
+    print('\nThe top momentum is zero in the top rest frame:')
     top_comf = top_momenta[i].boost_to_restframe(top_momenta[i]) 
     print(top_comf) # expected to be 0
-    print(math.sqrt(top_momenta[i]*top_momenta[i])) # invariant mass
+    # invariant mass
+    print('\nIn any frame, p^2=m^2 (invariant mass):')
+    print(math.sqrt(top_momenta[i]*top_momenta[i])) 
     # in the top frame, W+ and b are expected to have 
     bottom_comf = b_momenta[i].boost_to_restframe(top_momenta[i])
     wplus_comf = wplus_momenta[i].boost_to_restframe(top_momenta[i])
+    print('\nb and W+ momenta in the top rest frame:')
     print(bottom_comf)
     print(wplus_comf)
     break # stops at 1
-
